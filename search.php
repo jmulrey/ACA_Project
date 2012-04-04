810100ac0f7e2f59701f2492e5d44afd77257aa1
jmulrey <jmulrey@mail.umw.edu>
2012-04-04 14:16:19 -0400

    Mandatory message

--- search.php
+++ search.php
@@ -1,21 +1,27 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
-  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
-<head>
-  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-  <title>Search</title>
-  <link rel="stylesheet" type="text/css" href="style.css" />
-</head>
+<HTML>
+<HEAD>
+<TITLE>American Canoe Association / Home</TITLE>
+</HEAD>
+<table width = "100%">
+<tr>
+<td align = "left"><img src = "header.png" width = "100%"></td>
+</tr>
+</table>
+<table width = "100%" bgcolor=#4000FF>
+<tr>
+<td align = "right"><a href = "Home.php" style = "text-decoration: none"><font color=#FFFFFF >home</a> | <a href = "register.php" style = "text-decoration: none"><font color=#FFFFFF >register</font></a> | <a href = "aca_login.php" style = "text-decoration: none"><font color=#FFFFFF >login</font></font></a></td>
+</tr>
+</table>
 
-<body>
+<body bgcolor=#728FCE>
 <div id="contents">
   
   <?php
-  	session_start();
+  //	session_start();
 
   include "db_connect.php";
-  if (isset($_SESSION))
-  {
+ // if (isset($_SESSION))
+ // {
  
   	$searchterm = $_POST['search'];
     $searchterm = mysql_real_escape_string($searchterm);
@@ -42,7 +48,7 @@
 	    echo "</table>\n"; 
   	
   	
-  }
+  //}
   ?>
   <p>&nbsp;</p><p><a href="home.php">Back To Home</a></p>
   </div>
