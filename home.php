9206b5192880157a7e698d78c2fb2c9320957f0d
jmulrey <jmulrey@mail.umw.edu>
2012-04-04 14:41:14 -0400

    making changes to home.php

--- home.php
+++ home.php
@@ -13,6 +13,10 @@
 </tr>
 </table>
 <BODY bgcolor=#728FCE>
+<?php
+if(isset($_SESSION['username']))
+{
+echo('
 
 <table width = "100%">
 <td>
@@ -39,4 +43,38 @@
 </td>
 </table>
 </BODY>
+');
+}
+else
+{
+echo('
+
+<table width = "100%">
+<td>
+<H2 align=center><font color=#FFFFFF >Welcome to the American Canoe Association Website</font></H2>
+<H3 align=center><font color=#FFFFFF >Looking for a trail for your next trip or vacation? Search Below!</font></H3>
+<center>
+<form action = "search.php"\>
+<input type = "text" name = "search" size = "40"/>
+<input type = "submit" name = "submit" value = "search" /></td>
+</form>
+</center>
+</td>
+<td style = "background-color:#EEEEEE;width:300px;text-align:top;">
+<center>
+<form action = "aca_login.php"\>
+<h2><font color=#728FCE>Member Log In</font></h2>
+<h3 align=left><font color=#728FCE>Username</font></h3>
+<input type = "text" name = "username" size = "40"/>
+<h3 align=left><font color=#728FCE>Password</font></h3>
+<input type = "text" name = "password" size = "40"/>
+<input align=right type = "submit" name = "submit" value = "submit" /></td>
+</form>
+</center>
+</td>
+</table>
+</BODY>
+');
+}
+?>
 </HTML>
